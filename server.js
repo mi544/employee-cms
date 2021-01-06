bff759bb84b2b00b664e7a9668eba47dc9ac6e19
Maksim Verkhoturov <sd32@protonmail.com>
2021-01-06 01:01:03 -0700

    uses routes, hbs config

--- server.js
+++ server.js
@@ -6,8 +6,24 @@ const routes = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+const hbs = exphbs.create({
+  helpers: {
+    isId(column) {
+      return column === 'id'
+    },
+    print(context) {
+      console.log(context, '\n\n\n')
+      return 'printed'
+    }
+  },
+  defaultLayout: 'main',
+  extname: '.hbs'
+})
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(express.static('public'))
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('.hbs', hbs.engine)
 app.set('view engine', '.hbs')
 
 app.use(routes)
