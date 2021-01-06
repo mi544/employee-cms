b7759233b50b3e60a4f8f4f444c4aacf225d1a4a
Maksim Verkhoturov <sd32@protonmail.com>
2021-01-06 01:00:47 -0700

    renames column value in view

--- db/views.sql
+++ db/views.sql
@@ -1,11 +1,12 @@
-CREATE VIEW employee_main AS 
+CREATE VIEW employee_main AS
   SELECT
+    e.id,
     CONCAT(e.first_name, ' ', e.last_name) AS "Full Name",
     r.name AS "Role",
     r.salary AS "Salary",
     d.name AS "Department",
     CASE
-      WHEN e2.first_name IS NULL THEN 'No manager'
+      WHEN e2.first_name IS NULL THEN 'None'
       ELSE e2.first_name || ' ' || e2.last_name
     END 
     AS "Manager"
@@ -19,4 +20,17 @@ CREATE VIEW employee_main AS
   ON r.department_id = d.id
   
   LEFT JOIN employee AS e2
-  ON e.manager_id = e2.id
+  ON e.manager_id = e2.id;
+
+
+CREATE VIEW role_main AS
+  SELECT
+    r.id,
+    r.name AS "Role",
+    r.salary AS "Salary",
+    d.name AS "Department"
+
+  FROM role AS r
+
+  LEFT JOIN department AS d
+  ON r.department_id = d.id;
