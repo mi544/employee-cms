4823c06a0820836f231769c17185760bb2ac3e81
Maksim Verkhoturov <sd32@protonmail.com>
2021-01-06 23:34:42 -0700

    adds checks for all fields to be present

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,13 @@
 const db = require('../utils/db')
 const router = require('express').Router()
+const { fnErr, lnErr, rErr, mErr } = require('../utils/error-msgs')
 
 router.post('/employee', async function (req, res) {
   const { first_name, last_name, role, manager } = req.body
+  if (!first_name) return res.render('error', { message: fnErr, redirect: '/add' })
+  if (!last_name) return res.render('error', { message: lnErr, redirect: '/add' })
+  if (!role) return res.render('error', { message: rErr, redirect: '/add' })
+  if (!manager) return res.render('error', { message: mErr, redirect: '/add' })
   const result = await db.addEmployee(first_name, last_name, role, manager)
   console.log(result)
   res.status(200).redirect('/employee-add-success')
---
+++ utils/error-msgs/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  fnErr: 'First Name is missing.',
+  lnErr: 'Last Name is missing.',
+  rErr: 'Role is missing',
+  mErr: 'Manager is missing'
+}
